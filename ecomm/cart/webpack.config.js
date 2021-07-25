b092ec1efbea343643ccae5977d57a96dd0485d1
anhtudo97 <anhtudo97@gmail.com>
2021-07-25 17:19:23 +0700

    feat: add config cart

---
+++ ecomm/cart/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+    mode: "development",
+    devServer: {
+        compress: true,
+        port: 8082,
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: "cart",
+            filename: "remote-cart-entry.js",
+            exposes: {
+                "./CartIndex": "./src/index",
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+};
