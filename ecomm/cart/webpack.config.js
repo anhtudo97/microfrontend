d6aca8af982007a92d642197b977eff5c55e61b4
anhtudo97 <anhtudo97@gmail.com>
2021-07-27 22:35:03 +0700

    feat: add share module package

--- ecomm/cart/webpack.config.js
+++ ecomm/cart/webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
             exposes: {
                 "./CartIndex": "./src/index",
             },
+            shared: ["faker"],
         }),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
